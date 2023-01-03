93ea7a6f18dd2c4361a4a3e80ece0ce9119366d0
Gaspar Ibañez <gaspar.nahuel@hotmail.com>
2023-01-03 14:26:07 -0300

    actualizacion de archivos

---
+++ app.js
@@ -0,0 +1,36 @@
+import * as url from 'url';
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+dotenv.config(__dirname)
+const PORT = process.env.PORT || 8000 ;
+
+const app = express();
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.route('/register')
+    .get((req, res) => {
+        res.sendFile(path.resolve(__dirname, './views/register.html'));
+    })
+    .post((req, res) => {
+        res.sendFile(path.resolve(__dirname, './views/register.html'));
+    })
+
+app.route('/login')
+    .get((req, res) => {
+        res.sendFile(path.resolve(__dirname, './views/login.html'));
+    })
+    .post((req, res) => {
+        res.sendFile(path.resolve(__dirname, './views/login.html'));
+    })
+
+
+app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`)})
